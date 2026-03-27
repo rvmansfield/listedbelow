3a59872fb9b184162efbf700420dee325e67024d
rvmansfield <rvmansfield@gmail.com>
2026-03-27 10:22:14 -0400

    Updates to YAML for specific Python version

--- listedbelow/settings/base.py
+++ listedbelow/settings/base.py
@@ -44,7 +44,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware', 
 ]
 
 ROOT_URLCONF = 'listedbelow.urls'
